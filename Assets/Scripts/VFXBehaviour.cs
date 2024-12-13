634c3fd7675b1e5e43be14309006eb23bb5b11fa
Elizaveta Zalomnova <61969421+handlessdude@users.noreply.github.com>
2024-12-14 02:18:31 +0300

    add particle system

---
+++ Assets/Scripts/VFXBehaviour.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+public class VFXBehaviour : MonoBehaviour
+{
+    public ParticleSystem burstEffectPrefab;
+    private float PARTICLE_SYSTEM_DEFAULT_DURATION = 2f;
+    
+    public CartBehaviour cart;
+    
+    private void Start()
+    {
+        if (cart != null)
+        {
+            cart.OnHitObstacle += HandleHitObstacle;
+        }
+    }
+
+    private void HandleHitObstacle(Collision collision)
+    {
+        if (burstEffectPrefab != null)
+        {
+            // Get the contact point from the collision
+            Vector3 collisionPoint = collision.contacts[0].point;
+
+            if (burstEffectPrefab != null)
+            { 
+                var burstEffect = Instantiate(burstEffectPrefab, collisionPoint, Quaternion.identity);
+                ParticleSystem ps = burstEffect.GetComponent<ParticleSystem>();
+                if (ps != null)
+                {
+                    Destroy(burstEffect, ps.main.duration + ps.main.startLifetime.constantMax);
+                }
+                else
+                {
+                    Destroy(burstEffect, PARTICLE_SYSTEM_DEFAULT_DURATION);
+                }       
+            }
+        }
+    }
+    
+    private void OnDestroy()
+    {
+        if (cart != null)
+        {
+            cart.OnHitObstacle -= HandleHitObstacle;
+        }
+    }
+}
